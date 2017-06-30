bc5c32ef3fd1b5e01c70a99ae687b95ed89a876d
marcoantonio <fiusajr@gmail.com>
2017-06-30 10:41:10 -0300

    Sem comentários

--- app/src/main/java/com/example/assis/menugaveta/MainActivity.java
+++ app/src/main/java/com/example/assis/menugaveta/MainActivity.java
@@ -40,12 +40,6 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-        //Comentario Marco
-        //teste2
-        // Novo teste 2
-        // Sobe carai
-        // Mais um comentario
     }
 
     @Override
@@ -106,8 +100,4 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-
-    //negão da picona
-    //putaria
-    //testando
 }

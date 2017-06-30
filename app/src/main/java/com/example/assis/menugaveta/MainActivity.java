75d9d96d3c57efb355b3c7c9962bf70cbbda63ef
assismontenegro <assismontenegro@hotmail.com>
2017-06-30 10:11:59 -0300

    assis teste3

--- app/src/main/java/com/example/assis/menugaveta/MainActivity.java
+++ app/src/main/java/com/example/assis/menugaveta/MainActivity.java
@@ -105,3 +105,5 @@ public class MainActivity extends AppCompatActivity
     //putaria
     //testando
 }
+
+//TODO: se der erro, alterar no app com.google.android.gms:play-services-appindexing:9.6.1 por com.google.android.gms:play-services-auth:9.6.1
